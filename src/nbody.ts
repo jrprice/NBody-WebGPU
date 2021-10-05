7357ee63748c053946ae3ad23fa2ebd595c0ac8d
James Price <jrprice@google.com>
2021-10-05 14:29:25 -0400

    Add a compute shader to update the positions
    Hardcoded to just move all positions slowely to the right. The vertex
    stage now loads the positions via a vertex buffer.

--- src/nbody.ts
+++ src/nbody.ts
@@ -6,12 +6,23 @@ struct Float4Buffer {
 };
 
 [[group(0), binding(0)]]
-var<storage> positions : Float4Buffer;
+var<storage, read_write> positions : Float4Buffer;
+
+// TODO: Better workgroup size
+[[stage(compute), workgroup_size(1)]]
+fn cs_main(
+  [[builtin(global_invocation_id)]] gid : vec3<u32>,
+  ) {
+  let idx = gid.x;
+  // TODO: Implement N-Body logic.
+  positions.data[idx].x = positions.data[idx].x + 0.001;
+}
 
 [[stage(vertex)]]
 fn vs_main(
   [[builtin(instance_index)]] idx : u32,
   [[builtin(vertex_index)]] vertex : u32,
+  [[location(0)]] position : vec4<f32>,
   ) -> [[builtin(position)]] vec4<f32> {
 
   var vertexOffsets = array<vec2<f32>, 3>(
@@ -20,8 +31,7 @@ fn vs_main(
     vec2<f32>(0.0, 0.1),
   );
 
-  let pos = positions.data[idx];
-  return vec4<f32>(pos.xy + vertexOffsets[vertex], pos.zw);
+  return vec4<f32>(position.xy + vertexOffsets[vertex], position.zw);
 }
 
 [[stage(fragment)]]
@@ -32,6 +42,7 @@ fn fs_main() -> [[location(0)]] vec4<f32> {
 
 let device: GPUDevice = null;
 let queue: GPUQueue = null;
+let computePipeline: GPUComputePipeline;
 let renderPipeline: GPURenderPipeline;
 let canvas: HTMLCanvasElement = null;
 let canvasContext: GPUCanvasContext = null;
@@ -61,7 +72,7 @@ const init = async () => {
   ]);
   positionsBuffer = device.createBuffer({
     size: 3 * 4 * 4,
-    usage: GPUBufferUsage.STORAGE,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.VERTEX,
     mappedAtCreation: true
   });
   let positionsMapped = new Float32Array(positionsBuffer.getMappedRange());
@@ -72,10 +83,21 @@ const init = async () => {
   const module = device.createShaderModule({ code: wgsl });
 
   // Create the render pipeline.
+  const positionsAttribute: GPUVertexAttribute = {
+    shaderLocation: 0,
+    offset: 0,
+    format: 'float32x4',
+  };
+  const positionsLayout: GPUVertexBufferLayout = {
+    attributes: [positionsAttribute],
+    arrayStride: 4 * 4,
+    stepMode: 'instance',
+  };
   renderPipeline = device.createRenderPipeline({
     vertex: {
       module: module,
       entryPoint: 'vs_main',
+      buffers: [positionsLayout],
     },
     fragment: {
       module: module,
@@ -89,9 +111,17 @@ const init = async () => {
     },
   });
 
+  // Create the compute pipeline.
+  computePipeline = device.createComputePipeline({
+    compute: {
+      module: module,
+      entryPoint: 'cs_main',
+    },
+  });
+
   // Create the bind group.
   bindGroup = device.createBindGroup({
-    layout: renderPipeline.getBindGroupLayout(0),
+    layout: computePipeline.getBindGroupLayout(0),
     entries: [
       {
         binding: 0,
@@ -107,6 +137,14 @@ const init = async () => {
 
 // Render loop.
 const draw = () => {
+  const commandEncoder = device.createCommandEncoder();
+
+  const computePassEncoder = commandEncoder.beginComputePass();
+  computePassEncoder.setPipeline(computePipeline);
+  computePassEncoder.setBindGroup(0, bindGroup);
+  computePassEncoder.dispatch(3);
+  computePassEncoder.endPass();
+
   const colorTexture: GPUTexture = canvasContext.getCurrentTexture();
   const colorTextureView: GPUTextureView = colorTexture.createView();
   const colorAttachment: GPURenderPassColorAttachment = {
@@ -114,14 +152,13 @@ const draw = () => {
     loadValue: { r: 0, g: 0, b: 0, a: 1 },
     storeOp: 'store'
   };
-  const commandEncoder = device.createCommandEncoder();
   const renderPassEncoder = commandEncoder.beginRenderPass({
     colorAttachments: [colorAttachment],
   });
   renderPassEncoder.setPipeline(renderPipeline);
   renderPassEncoder.setViewport(0, 0, canvas.width, canvas.height, 0, 1);
   renderPassEncoder.setScissorRect(0, 0, canvas.width, canvas.height);
-  renderPassEncoder.setBindGroup(0, bindGroup);
+  renderPassEncoder.setVertexBuffer(0, positionsBuffer);
   renderPassEncoder.draw(3, 3);
   renderPassEncoder.endPass();
 
